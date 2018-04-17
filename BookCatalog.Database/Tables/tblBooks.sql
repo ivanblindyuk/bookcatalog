39287b108183220c5e6cefe2c85a6015060d4344
ivanblindyuk <ivanblindyuk@mail.ru>
2018-04-17 21:43:09 +0300

    Created approximate structure of the project

--- BookCatalog.Database/Tables/tblBooks.sql
+++ BookCatalog.Database/Tables/tblBooks.sql
@@ -2,6 +2,6 @@
 (
 	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	[Title] NVARCHAR(max) NOT NULL,
-	[AuthorId] INT NOT NULL,
+	[AuthorId] INT NULL,
 	CONSTRAINT [FK_tblBooks_tblAuthors] FOREIGN KEY ([AuthorId]) REFERENCES [tblAuthors] ([Id])
 )
