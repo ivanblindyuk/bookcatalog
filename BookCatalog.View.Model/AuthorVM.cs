413d05c331ca808a80eb16c9ef2b52fe258928e9
ivanblindyuk <ivanblindyuk@mail.ru>
2018-05-07 23:15:09 +0300

    Implemented CRUD methods of AuthorsController

--- BookCatalog.View.Model/AuthorVM.cs
+++ BookCatalog.View.Model/AuthorVM.cs
@@ -8,6 +8,7 @@ namespace BookCatalog.View.Model
 {
     public class AuthorVM
     {
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
     }
