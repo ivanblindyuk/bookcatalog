98f1f3033cb73217163d4f3c5d278912086e8520
ivanblindyuk <ivanblindyuk@mail.ru>
2018-05-08 17:16:47 +0300

    altered tables tblBooks and tblBooks_Authors

--- BookCatalog.Database/Tables/tblBooks.sql
+++ BookCatalog.Database/Tables/tblBooks.sql
@@ -3,5 +3,7 @@
 	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	[Title] NVARCHAR(max) NOT NULL,
 	[ReleaseDate] DATE NULL,
-	[Ranking] INT NULL
+	[Ranking] INT NULL,
+	[PageCount] INT NULL,	
+	CONSTRAINT [CHK_tblBooks_RankingValue] CHECK ([Ranking] > 0 AND [Ranking] <= 10) 
 )
--- BookCatalog.Database/Tables/tblBooks_Authors.sql
+++ BookCatalog.Database/Tables/tblBooks_Authors.sql
@@ -3,6 +3,6 @@
 	[Id] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
 	[BookId] INT NOT NULL,
 	[AuthorId] INT NOT NULL,
-	CONSTRAINT [FK_tblBooks_Authors_tblBooks] FOREIGN KEY ([BookId]) REFERENCES [tblBooks] ([Id]),
+	CONSTRAINT [FK_tblBooks_Authors_tblBooks] FOREIGN KEY ([BookId]) REFERENCES [tblBooks] ([Id]) ON DELETE CASCADE,
 	CONSTRAINT [FK_tblBooks_Authors_tblAuthors] FOREIGN KEY ([AuthorId]) REFERENCES [tblAuthors] ([Id])
 )
